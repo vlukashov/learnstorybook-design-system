adb6b987825b86f783f24d8f9d3ab303f4ebecad
Viktor Lukashov <viktor@stepzen.com>
2022-06-14 13:35:16 +0300

    feat: add StepZen theme

---
+++ .storybook/StepZenTheme.js
@@ -0,0 +1,12 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+
+  appBg: "linear-gradient(180deg, #e2fcfe 38.72%, #ede2ff 134.84%);",
+
+  brandTitle: "StepZen Storybook",
+  brandUrl: "https://stepzen.com/",
+  brandImage: "https://stepzen.com/images/logo.svg",
+  brandTarget: "_self",
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import stepZenTheme from "./StepZenTheme";
+
+addons.setConfig({
+  theme: stepZenTheme,
+});
